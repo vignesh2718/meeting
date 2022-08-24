0d7199b1175bdfc99662fb5c1678b6a07d48cd9e
vignesh2718 <vigneshn2718@gmail.com>
2022-08-24 15:14:43 +0530

    Meeting version 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+// eslint-disable-next-line no-unused-vars
+import {Route, Routes} from 'react-router-dom';
+import Home from './pages/Home';
+import AddMeetings from './pages/AddMeeting';
+import Navigation from './components/Navigation';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+
+        <Navigation />
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/add' element={<AddMeetings />} />
+        </Routes>
+
+
+
+      </div>
   );
 }
 
